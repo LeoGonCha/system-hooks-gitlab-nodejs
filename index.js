8ad2b983562b252b2eac37bd50a1dfaee68c86ad
LeoGonCha <leogoncha@gmail.com>
2018-08-03 08:48:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-var express = require('express')
-  , app = express.createServer();
+const express = require('express')
+let app = express();
 
-app.use(express.bodyParser());
+app.configure(function(){
+   app.use(express.static("."));
+   app.use(express.bodyParser());
+   app.use(express.methodOverride());
+ });
 
-app.post('/', function(request, response){
-  console.log(req.params)
-  console.log(req.body)
-  console.log('request =' + JSON.stringify(req.body))
-  res.end('Ok');
-});
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post("/", (req,res)=>{
+    console.log(req.body)
+    console.log(req.params)
+})
 
 app.listen("3000", ()=>{
     console.log("Server is listening on port 3000")
